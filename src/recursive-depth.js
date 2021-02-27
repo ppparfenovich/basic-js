848d8a115bf1d5950314051bbc0ec1232274e9a3
Pavel <ppparfenovich@gmail.com>
2021-02-27 12:12:48 +0300

    fix: depth & transform arr solutions

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+
+    let maxDepth = 1;
+
+    for (let i = 0; i < arr.length; i++) {
+      if (Array.isArray(arr[i])) {
+        let depth = 1;
+        depth += this.calculateDepth(arr[i]);
+        if (maxDepth < depth) {
+          maxDepth = depth;
+        }
+      }
+    }
+    return maxDepth;
   }
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,21 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-  let doubleArr = arr;
   let newArr = [];
-  if (Array.isArray(doubleArr) !== true) {
-    throw 'THROWN';
+  if(!Array.isArray(arr)){
+    throw new Error();
   }
-  for (let i = 0; i < doubleArr.length; i++) {
-    if (doubleArr[i] === '--double-prev') {
-      newArr.push(doubleArr[i - 1])
-    } else if (doubleArr[i] === '--double-next') {
-      newArr.push(doubleArr[i + 1])
-    } else if (doubleArr[i] === '--discard-prev') {
-      newArr.pop()
-    } else if (doubleArr[i] === '--discard-next') {
-      i++
-    }
+
+  for (let i=0;i<arr.length;i++){
+    switch(arr[i]){
+      case '--discard-next':
+        i++;
+        break;
+      case '--discard-prev':
+        if (i > 0 && arr[i - 2] != '--discard-next') {    // выполняем если i не 0 и если на 2 эл. назад нету '--discard-next'
+          newArr.pop()
+        }
+        break;
+      case '--double-prev':
+        if (i > 0 && arr[i-2] !='--discard-next') {       // выполняем если i не 0 и если на 2 эл. назад нету '--discard-next'
+          newArr.push(arr[i-1])
+        }
+        break;
+      case '--double-next':
+        if (i < arr.length - 1) {
+          newArr.push(arr[i+1]);
+        }
+        break;
+      default :
+      newArr.push(arr[i]);
+    }    
   }
   return newArr
 };

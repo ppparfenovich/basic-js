da28b1c84a9a634fe806d36df031ecbc4387b3e6
Pavel <ppparfenovich@gmail.com>
2021-03-01 12:45:41 +0300

    feat: add solution to repeaters

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,48 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+
+  let strPlusAddition = '';
+  strPlusAddition = String(str);    //глобальный объект String 
+
+  let addition = ''; 
+  if (options.addition !== undefined) {
+    addition = String(options.addition);
+  }
+
+  let additionRepeatTimes = 1;
+  if (options.additionRepeatTimes !== undefined) {
+    additionRepeatTimes = options.additionRepeatTimes;
+  }
+
+  let additionSeparator = '|';
+  if (options.additionSeparator !== undefined) {
+    additionSeparator = String(options.additionSeparator);
+  }
+
+  for (let i = 1; i <= additionRepeatTimes; i++) {
+    strPlusAddition = strPlusAddition + addition
+    if (i < additionRepeatTimes) {
+      strPlusAddition = strPlusAddition + additionSeparator;
+    } 
+  }
+
+  let result = strPlusAddition;
+
+  let repeatTimes = 1;
+  if (options.repeatTimes !== undefined) {
+    repeatTimes = options.repeatTimes;
+  }
+  let separator = '+'; 
+  if (options.separator !== undefined) {
+    separator = String(options.separator);
+  }
+
+  for (let j = 1; j <= repeatTimes; j++) {
+    if (j < repeatTimes) {
+      result = result + separator + strPlusAddition;
+    }
+  }
+  return result;
 };
   
